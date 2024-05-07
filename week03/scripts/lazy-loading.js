7bba245dd44c553952c2afbf10f8147e6b049fa1
DiegoBellido99 <dbellidoochoa@gmail.com>
2024-05-07 11:55:24 -0500

    lazy-loading

---
+++ week03/scripts/lazy-loading.js
@@ -0,0 +1,38 @@
+const lastModified= document.querySelector("#modified");
+
+const change = document.lastModified;
+
+let dateChange = new Date(change);
+
+let day = dateChange.getDate();
+let month =dateChange.getMonth() + 1;
+let year = dateChange.getFullYear();
+let hours = dateChange.getHours();
+let minutes =dateChange.getMinutes();
+let seconds = dateChange.getSeconds();
+
+if (day < 10){
+    day = '0' + day ;
+}
+
+if(month < 10){
+
+    month = '0'+ month;
+}
+
+if(hours < 10){
+
+    hours = '0'+ hours;
+}
+
+if (minutes < 10){
+
+    minutes= '0' + minutes;
+}
+
+if (seconds < 10){
+
+    seconds = '0' + seconds;
+}
+
+lastModified.innerHTML = `Last Modification : ${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
