147fecad0b5bcf38dda971b0bfb6020f742dea80
DiegoBellido99 <dbellidoochoa@gmail.com>
2024-06-03 23:41:36 -0500

    fixing

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -68,12 +68,12 @@ var date = new Date();
 var difference= date - new Date(lastVisit);
 var daysDifference = Math.floor(difference / (1000 * 60 * 60 * 24));
 
-
+/*
 if(number!==0 && daysDifference ===0){
     visits.innerHTML=`Back so soon! Awesome!!!! <br>  Number of visits : ${number}`;
 }
 else if(number!==0 && daysDifference>=1){
-    visits.textContent=`Number of visits :${number}`;
+    visits.innerHTML=`Number of visits :${number}`;
 }
 else{
     visits.textContent=`Welcome! Let us know if you have any questions.`;
@@ -83,8 +83,125 @@ number++
 
 localStorage.setItem("numVisits-ls",number);
 localStorage.setItem("lastVisit", new Date());
+*/
+document.addEventListener("DOMContentLoaded", function() {
+    const eventDiv = document.getElementById("event");
+    const today = new Date().getDay(); 
+
+    if (today === 1 || today === 2 || today === 3) {
+        eventDiv.style.display = "block"; 
+    }
+});
+
+const close=document.addEventListener("click",()=>{
+    const event=document.querySelector("#event");
+    event.style.display="none";
+})
 
 
 
 
+const url ="https://api.openweathermap.org/data/2.5/weather?lat=-12.04&lon=-77.03&&units=imperial&appid=5f5ebe8f821ecc05261398d4caf43542"
 
+async function getWeather(){
+    try{
+        const response = await fetch(url);
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayWeather(data);
+        }else{
+            throw Error(await response.text());
+        }
+    }catch(error){
+        console.log(error);
+
+    }
+}
+
+function displayWeather(data){
+
+    const city =document.querySelector(".city");
+    const temperature=document.querySelector(".temperature");
+    const humidity =document.querySelector(".humidity");
+    const image=document.querySelector("#weather-img");
+
+    const icon =`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    
+    city.textContent = data.name;
+    temperature.innerHTML=`Temperature : ${data.main.temp} F°`;
+    humidity.innerHTML=`Humidity : ${data.main.humidity}%`;
+
+    image.setAttribute("src",icon);
+
+}
+
+
+getWeather();
+
+
+
+const urlForecast = "http://api.openweathermap.org/data/2.5/forecast?lat=-12.04&lon=-77.03&units=imperial&appid=5f5ebe8f821ecc05261398d4caf43542";
+
+async function getForecast() {
+    try {
+        const response1 = await fetch(urlForecast);
+        if (response1.ok) {
+            const data1 = await response1.json();
+            console.log(data1);
+            displayForecast(data1)
+        } else {
+            throw Error(await response1.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayForecast(data){
+    
+    const forecast=document.querySelector("#forecast");
+
+    const tomorrow=document.createElement("div");
+
+    const day=document.createElement("h3");
+    const tempTomorrow=document.createElement("p");
+    const descriptionTomorrow=document.createElement("p");
+    const iconTomorrow=document.createElement("img");
+    
+    day.textContent=`Tomorrow`;
+    tempTomorrow.innerHTML=data.list[3].main.temp;
+    descriptionTomorrow.innerHTML=data.list[3].weather[0].description;
+    const icon1 =`https://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png`;
+    iconTomorrow.setAttribute("src",icon1);
+ 
+    tomorrow.appendChild(day);
+    tomorrow.appendChild(tempTomorrow);
+    tomorrow.appendChild(descriptionTomorrow);
+    tomorrow.appendChild(iconTomorrow);
+
+
+
+    const tomorrow2=document.createElement("div");
+    const day2=document.createElement("h3");
+    const tempTomorrow2=document.createElement("p");
+    const descriptionTomorrow2=document.createElement("p");
+    const iconTomorrow2=document.createElement("img");
+
+    day2.textContent=`Day after tomorrow`
+    tempTomorrow2.innerHTML=data.list[10].main.temp;
+    descriptionTomorrow2.innerHTML=data.list[10].weather[0].description;
+    const icon2 =`https://openweathermap.org/img/wn/${data.list[10].weather[0].icon}.png`;
+    iconTomorrow2.setAttribute("src",icon2);
+
+    tomorrow2.appendChild(day2);
+    tomorrow2.appendChild(tempTomorrow2);
+    tomorrow2.appendChild(descriptionTomorrow2);
+    tomorrow2.appendChild(iconTomorrow2);
+
+    forecast.appendChild(tomorrow);
+    forecast.appendChild(tomorrow2);
+
+}
+
+getForecast();
